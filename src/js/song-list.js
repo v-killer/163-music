6535d7ee3579e818478a7622e3b95f7427ca13ca
shen <343206898@qq.com>
2018-10-20 15:16:37 +0800

    添加 loading

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -8,29 +8,28 @@
         render(data) {
             let $el = $(this.el)
             $el.html(this.template)
-            let {
-                songs
-            } = data
-            let liList = songs.map((song) => 
-                $('<li></li>').text(song.name).attr('data-song-id',song.id))
+            let { songs, selectSongId } = data
+            let liList = songs.map((song) => {
+                let $li = $('<li></li>').text(song.name).attr('data-song-id', song.id)
+                if(song.id === selectSongId){
+                    $li.addClass('active')
+                }
+                return $li
+            })
+
             $el.find('ul').empty()
             liList.map((domLi) => {
                 $el.find('ul').append(domLi)
             })
         },
-        activeItem(li){
-            let $li = $(li)
-            $li.addClass('active')
-                .siblings('.active').removeClass('active')
-        },
-
         clearActive() {
             $(this.el).find('.active').removeClass('active')
         }
     }
     let model = {
         data: {
-            songs: []
+            songs: [],
+            selectSongId: undefined
         },
         find() {
             var query = new AV.Query('Song');
@@ -54,24 +53,27 @@
             this.bindEventHub()
             this.getAllSongs()
         },
-        getAllSongs(){
+        getAllSongs() {
             return this.model.find().then(() => {
                 this.view.render(this.model.data)
             })
         },
         bindEvents() {
-            $(this.view.el).on('click','li',(e)=>{
-                this.view.activeItem(e.currentTarget)
+            $(this.view.el).on('click', 'li', (e) => {
                 let songId = e.currentTarget.getAttribute('data-song-id')
+                
+                this.model.data.selectSongId = songId
+                this, view.render(this.model.data)
+
                 let data
                 let songs = this.model.data.songs
-                for(let i=0;i<songs.length;i++){
-                    if(songs[i].id === songId){
+                for (let i = 0; i < songs.length; i++) {
+                    if (songs[i].id === songId) {
                         data = songs[i]
                         break
                     }
                 }
-                window.eventHub.emit('select',JSON.parse(JSON.stringify(data))) // 深拷贝
+                window.eventHub.emit('select', JSON.parse(JSON.stringify(data))) // 深拷贝
             })
         },
         bindEventHub() {
@@ -79,14 +81,14 @@
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
             })
-            window.eventHub.on('new',()=>{
+            window.eventHub.on('new', () => {
                 this.view.clearActive()
             })
-            window.eventHub.on('update',(song)=>{
+            window.eventHub.on('update', (song) => {
                 let songs = this.model.data.songs
-                for(let i=0;i<songs.length;i++){
-                    if(songs[i].id === song.id){
-                        Object.assign(songs[i],song)
+                for (let i = 0; i < songs.length; i++) {
+                    if (songs[i].id === song.id) {
+                        Object.assign(songs[i], song)
                     }
                 }
                 this.view.render(this.model.data)
